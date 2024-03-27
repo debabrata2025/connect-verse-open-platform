cdb915a87bf0c9b50990df1b539de3e3f99c768b
Debabrata santra <98827530+Radhegit@users.noreply.github.com>
2024-03-27 14:48:08 +0530

    Add files via upload

--- community.php
+++ community.php
@@ -10,8 +10,9 @@ if (!isset ($_SESSION['name'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QNA</title>
+    <title>community</title>
     <link rel="stylesheet" href="quora.css?v=<?php echo time(); ?>">
+    <link rel="icon" type="image/png" href="com.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
